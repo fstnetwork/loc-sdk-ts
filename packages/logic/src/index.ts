ef20f5580f7f4683993af1205c6a06f5e52d495d
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2022-08-17 11:18:49 +0800

    refactor: remove unused code

--- packages/logic/src/index.ts
+++ packages/logic/src/index.ts
@@ -8,7 +8,6 @@ export {
   MessageQueuePayload,
 } from "./logic";
 
-export const Database = Saffron.Database;
 export const Http = Saffron.Http;
 export const Smtp = Saffron.Smtp;
 export const Parameter = Saffron.Parameter;

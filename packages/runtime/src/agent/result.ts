0b76eca11eecf32ca2bbdb4006c6e3a353e0b6dd
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2022-10-30 22:26:34 +0800

    feat(runtime): remove `opSync` from `Deno.core`

--- packages/runtime/src/agent/result.ts
+++ packages/runtime/src/agent/result.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const ResultAgent = {
   finalize(value: object): void {
-    Deno.core.opSync('op_result_agent_set', value);
+    Deno.core.ops['op_result_agent_set']?.(value);
   },
 };
